76efe84aef66c0c3a4e510cd5d5f5799d371936c
Linda Hansson <lhansson644@gmail.com>
2025-12-18 09:28:40 +0100

    created vis4 and added to jupyter notebook

---
+++ data/vis4.sql
@@ -0,0 +1,9 @@
+
+
+SELECT
+    YEAR(OrderDate) AS OrderYear,
+    SUM(TotalDue) AS TotalSales,
+    COUNT(SalesOrderID) AS OrderCount
+FROM Sales.SalesOrderHeader
+GROUP BY YEAR(OrderDate)
+ORDER BY OrderYear;
